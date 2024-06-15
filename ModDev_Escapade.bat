40463c7c40128ee645f2a3b16abfb8af6281164f
kiraimmortalhu <kiraimmortalhu@gmail.com>
2024-06-15 13:34:54 +0200

    Changed bat file path, added empty ballroom map

--- ModDev_Escapade.bat
+++ ModDev_Escapade.bat
@@ -1,3 +1,3 @@
 cd /d ..
 cd /d ..
-AmnesiaTheBunker.exe -user Dev -cfg config/main_init_dev.cfg -mod local:Stronghold_Escapade
+AmnesiaTheBunker.exe -user Dev -cfg config/main_init_dev.cfg -mod local:escapade_hpl3
